cc9c8d2993c0b6bf29e6dff89f90ebc532ba2b23
Dwsysfx <84414140+dwsysfx@users.noreply.github.com>
2022-12-29 15:48:56 -0600

    Updated help command

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,14 @@ module.exports = {
             .setTitle('Lobby 55 Help')
             .setDescription('Need help? DM OverFallen or Dwsysfx for support.')
             .addFields(
-                { name: 'Verify', value: 'To get your guild roles you must connect your Discord to your minecraft account using this bot. To do this run the </verify:1057545909279399988> command.'}
+                { name: 'Verify', value: 'To get your guild roles you must connect your Discord to your minecraft account using this bot. To do this run the `/verify\` command.' },
+                { name: '‎', value: '‎', inline: true},
+                { name: 'Unverify', value: 'To unlink your account and remove your data you can run the command \`/unverify\`' },
+                { name: '‎', value: '‎', inline: true},
+                { name: 'Ping', value: 'Pong! Get the ping of the bot and the DJS api.' }
             )
+            .setFooter({ text: `${interaction.user.username}`, iconURL: `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}` })
+            .setTimestamp()
+        interaction.reply({ embeds: [helpEmbed] })
     }
 }
